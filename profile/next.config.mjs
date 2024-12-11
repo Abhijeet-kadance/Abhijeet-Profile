6269d5f92d34bacc5e270892e0a4987446ce82b5
Abhjeet Gangadhar thorat <87606140+Abhijeet-kadance@users.noreply.github.com>
2024-12-12 01:19:30 +0530

    Updated

--- profile/next.config.mjs
+++ profile/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // Enable static export mode
+    images: {
+      unoptimized: true, // Disable image optimization for static export
+    },
+    trailingSlash: true, // Ensure URLs have a trailing slash, useful for GitHub Pages
+  };
+  
+  export default nextConfig;
+  
